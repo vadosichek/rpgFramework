9c479ec1279edee6796a972711c3702c92af175e
Vad'OS <big-vadim@mail.ru>
2015-07-20 01:15:02 +0300

    GUI для диалога, скрипты для npc и игрока, для базового взаимодействия.

---
+++ Assets/Scripts/Npc scripts/AnswerButtonScript.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnswerButtonScript : MonoBehaviour {
+	public DialogScript dialog;
+	public int index;
+	public void setIndex(){
+		dialog.index = index;
+	}
+}
---
+++ Assets/Scripts/Npc scripts/NpcScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NpcScript : MonoBehaviour {
+	public PlayerScript player;
+	public float activeZoneRadius;
+	public bool passive;
+	public bool activated;
+	Vector3 playerPos;
+	void Update(){
+		player = PlayerScript.instance;
+		if(player){
+			playerPos = new Vector3(player.transform.position.x,transform.rotation.y,player.transform.position.z);
+			if(Vector3.Distance(transform.position,playerPos) <= activeZoneRadius){
+				if(!passive){Debug.Log("");}
+				activated = true;
+			}
+			else{
+				activated = false;
+			}
+		}
+	}
+
+	void OnDrawGizmos(){
+		Gizmos.color = Color.yellow;
+		Gizmos.DrawWireSphere(transform.position, activeZoneRadius);
+	}
+}
