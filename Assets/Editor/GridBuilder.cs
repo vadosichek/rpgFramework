08180e061f65d49598f3e6fa7bb75683710db371
Vad'OS <big-vadim@mail.ru>
2015-07-19 05:20:38 +0300

    система диалогов
    Добавлена базовая система вывода диалога

--- Assets/Editor/GridBuilder.cs
+++ Assets/Editor/GridBuilder.cs
@@ -1,52 +1,50 @@
 ﻿using UnityEngine;
 using UnityEditor;
 using System.Collections;
-
 public class GridBuilder : EditorWindow {
-	Transform gridPart;
-	Color baseColor = Color.black;
-	Color onMouseColor = Color.red;
-	int width=0,height=0;
-	Transform gridParent;
-	[MenuItem ("FrameWork/Grid building")]
-	static void Init(){
+	int index = 1;
+	string dialog = 
+		"T1/Hello, player 1/T1 2A/1/1/Hello, npc!/A/1/1"+
+		"T2/Hello, player 2/T2 1A/2/1/Hello, npc 2!/A/2/1";
+	// Window has been selected
+	[MenuItem ("FrameWork/Dialog builder")]
+	static void Init () {
+		// Get existing open window or if none, make a new one:
 		GridBuilder window = (GridBuilder)EditorWindow.GetWindow (typeof (GridBuilder));
 		window.Show();
 	}
-	int x=0;
-	void OnGUI () {
-		if(x==0){
-			if(GUILayout.Button("Build grid")){x=1;}
-			if(GUILayout.Button("Grid settings")){x=2;}
-		}
-		else if(x==1){
-			GUILayout.Label("Building grid");
-			gridPart = EditorGUILayout.ObjectField("Grid block",gridPart,typeof(Transform),true) as Transform;
-			gridParent = EditorGUILayout.ObjectField("Grid parent",gridParent,typeof(Transform),true) as Transform;
-			width = EditorGUILayout.IntField("Grid width",width);
-			if(width%2!=0){width--;}
-			height = EditorGUILayout.IntField("Grid height",height);
-			if(height%2!=0){height--;}
-			baseColor = EditorGUILayout.ColorField("Grid block base color",baseColor);
-			onMouseColor = EditorGUILayout.ColorField("Grid block on mouse over color",onMouseColor);
-			if(gridParent && gridPart){
-				if(GUILayout.Button("Build grid")){
-					x=0;
+	void OnGUI(){
+		GUILayout.Label ("Building dialog");
+		index = EditorGUILayout.IntField (index);
+
+		GUILayout.Label (
+			dialog.Substring(dialog.IndexOf("T"+index)+("T"+index).Length+1,
+		                 (dialog.IndexOf("/T"+index)-("/T"+index).Length*2)-dialog.IndexOf("T"+index)+("T"+index).Length+1)
+			);
+
+		for(int x=1;x<=2;x++){
+			if(dialog.IndexOf ("A/"+index+"/"+x+"/")!=-1){
+				if(GUILayout.Button(
+					dialog.Substring(dialog.IndexOf ("A/"+index+"/"+x+"/")+("A/"+index+"/"+x+"/").Length,
+				    dialog.IndexOf ("/A/"+index+"/"+x) - ("/A/"+index+"/"+x).Length*2 - dialog.IndexOf ("A/"+index+"/"+x+"/")+("A/"+index+"/"+x+"/").Length
+			        ))){
+					index=int.Parse(dialog.Substring(dialog.IndexOf ("/T"+index) + ("/T"+index).Length +1,dialog.IndexOf ("A/"+index+"/"+x+"/") - dialog.IndexOf ("/T"+index) - ("/T"+index).Length -1));
 				}
 			}
-			else{
-				GUILayout.Space(40);
-				GUILayout.Label("Select grid parent and grid part");
-			}
+		}
 
-		}
-		else if(x==2){
-			GUILayout.Label("Changing grid block settings");
-			baseColor = EditorGUILayout.ColorField("Grid block base color",baseColor);
-			onMouseColor = EditorGUILayout.ColorField("Grid block on mouse over color",onMouseColor);
-			if(GUILayout.Button("Change grid block settings")){
-				x=0;
-			}
-		}
 	}
+
+	/*void OnFocus() {
+		SceneView.onSceneGUIDelegate -= this.OnSceneGUI;
+		SceneView.onSceneGUIDelegate += this.OnSceneGUI;
+	}
+	
+	void OnDestroy() {
+		SceneView.onSceneGUIDelegate -= this.OnSceneGUI;
+	}
+	
+	void OnSceneGUI(SceneView sceneView) {
+		Handles.Button (new Vector3 (0, 0, 0), Quaternion.Euler (0, 0, 0), 10, 10, Handles.CircleCap);
+	}*/
 }
